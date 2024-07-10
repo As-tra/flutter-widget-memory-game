107e6bb75f62ad6ecb9d7705e8cd0a45eb08dcc1
Assil <astradhdbjd@gmail.com>
2024-07-10 20:59:28 +0100

    add firebase hosting

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets_memory_test/constants.dart';
 import 'package:flutter_widgets_memory_test/views/home_view.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const FlutterWidgetsMemoryGame());
 }
 
